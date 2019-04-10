8744bc908482f7b9717253dc6ea723c7d50250b7
Israel Hernandez Herrera <israelhhh2@gmail.com>
2019-04-10 10:29:13 -0700

    Added project started files

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var quotes = [
+    { quote: 'Quote 1', Author: 'Author 1' },
+    { quote: 'Quote 2', Author: 'Author 2' },
+    { quote: 'Quote 3', Author: 'Author 3' },
+    { quote: 'Quote 4', Author: 'Author 4' }
+];
+
+console.log(quotes[0][0]);
