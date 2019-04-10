7950ceac95e99377c6d62ceccf23c961256e1488
Israel Hernandez Herrera <israelhhh2@gmail.com>
2019-04-10 14:59:25 -0700

    Finished Main Objective

--- js/script.js
+++ js/script.js
@@ -3,54 +3,65 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+// Creating Array that store quote objects. 
+let quotes = [
+  {
+    quote: 'Only those who never stand up never fall down.',
+    source: 'John Kavanagh',
+    citation: 'Win or Learn',
+    year: 2016
+  },
+  {
+    quote: 'There is no talent here, this is hard work. This is an obsession.',
+    source: 'Connor Mcgregor',
+  },
+  {
+    quote: 'Sometimes you have to run before you can walk.',
+    source: 'Tony Stark',
+    citation: 'Iron Man',
+    year: 2008
+  },
+  {
+    quote: 'One of the most fascinating lessons i\'ve absorbed about life is that struggle is good.',
+    source: 'Joe Rogan'
+  },
+  {
+    quote: 'If something is important enough you should try even if the probable income is faulure.',
+    source: 'John Kavanagh',
+    citation: 'Win or Learn',
+    year: 2016
+  }
+];
+
+// Will get a random quote from the quotes array
+function getRandomQuote() {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  // Using the randomNumber value to select a random quote
+  return quotes[randomNumber];
+}
+
+// Test to see if all quotes can be selected
+// console.log(getRandomQuote());
+
+// function will add html to display the different quotes
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  let html = '';
+
+  html = `<p class="quote"> ${randomQuote.quote} </p>`;
+  html += `<p class="source"> ${randomQuote.source} `;
+
+  //Check if the quotes have citation or year property 
+  if (randomQuote.citation !== undefined) {
+    html += `<span class="citation">${randomQuote.citation}</span>`;
+  }
+  if (randomQuote.year !== undefined) {
+    html += `<span class="year">${randomQuote.year}</span></p>`;
+  }
+
+  document.querySelector('#quote-box').innerHTML = html;
+}
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
+// Will change change quote.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
